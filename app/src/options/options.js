1a27f26a4732725063c9d4947b907b6c1106efee
imaginebreake <breakertt@outlook.com>
2019-12-12 20:40:13 +0000

    Fix bug#36 and Add auto save

--- app/src/options/options.js
+++ app/src/options/options.js
@@ -2,53 +2,82 @@
  * Save Options
  */
 function saveOptions() {
-  var enableHD = document.getElementById('hdenable').value;
+  var enableHD = $('#hdenable input')[0].checked;
+  enableHD = !enableHD;
   chrome.storage.sync.set({
-      enableHD: enableHD,
-  }, function() {
+    enableHD: enableHD,
+  }, function () {
     // Update status to let user know options were saved.
     displayFinished('Saved Settings!');
+    updateCheckBox("hdenable", enableHD);
+    console.log("Save: " + "enableHD:" + enableHD);
   });
 }
 
 /**
  * Resets Options to default sttings
  */
-function resetOptions(){
+function resetOptions() {
+  var enableHDDefault = true;
   // Set Chrome Values
   chrome.storage.sync.set({
-      enableHD: false
-  }, function() {
-    // Update status to let user know options were saved.
+    enableHD: enableHDDefault,
+  }, function () {
+    updateCheckBox("hdenable", enableHDDefault);
     displayFinished('Resetted Settings to Default!');
+    console.log("Reset: " + "enableHD:true");
   });
-  document.getElementById('hdenable').checked = false;
-  displayFinished('Resetted Settings to Default!');
-  location.reload();
-}
+}/*  */
 
 /**
  * reloads currently saved options into values
  */
 function restoreOptions() {
   // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get({
-      enableHD: false,
-  }, function(items) {
-    document.getElementById('hdenable').checked = items.enableHD;
-  });
+  chrome.storage.sync.get(
+    ['enableHD'], function (items) {
+      updateCheckBox("hdenable", items.enableHD);
+      displayFinished('Config Loaded!');
+      console.log("Restore: " + "enableHD:" + items.enableHD);
+    });
 }
 
-function displayFinished(showMessage){
-    var snackbarContainer = document.querySelector('#demo-toast-example');
-    var data = {message: showMessage};
-    snackbarContainer.MaterialSnackbar.showSnackbar(data);
+function displayFinished(showMessage) {
+  var snackbarContainer = document.querySelector('#demo-toast-example');
+  var data = { message: showMessage };
+  snackbarContainer.MaterialSnackbar.showSnackbar(data);
 }
 
+/**
+ * Check and uncheck
+ */
+function updateCheckBox(id, checked) {
+  var checkBox = document.getElementById(id);
+  if (checked == true) {
+    checkBox.MaterialCheckbox.check();
+  } else {
+    checkBox.MaterialCheckbox.uncheck();
+  }
+}
 
-document.addEventListener('DOMContentLoaded', restoreOptions);
-document.getElementById('saveOptions').addEventListener('click',
-    saveOptions);
+/**
+ * Initialize or restore options
+ */
+document.addEventListener('DOMContentLoaded', function () {
+  chrome.storage.sync.get(
+    ['enableHD'], function (items) {
+      if (items.enableHD === undefined) {
+        // initialize
+        resetOptions();
+      } else {
+        // restore
+        restoreOptions();
+      }
+    });
+});
 
 document.getElementById('resetOptions').addEventListener('click',
-    resetOptions);
+  resetOptions);
+
+document.getElementById('hdenable').addEventListener('click',
+  saveOptions);
